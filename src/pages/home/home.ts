f3948b3ab661fb27dc7211db0cf46b74efc48eeb
DuffyTJ89 <tomduffy89@gmail.com>
2018-02-24 17:57:01 +0000

    basic app with barCode scan in ionic 3 added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import{BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  options: BarcodeScannerOptions; //options variable of type BarcodeScannerOptions
+  resultsUI:{}; //used to dispaly the scanned results in the user interface 
 
+  constructor(private barcode: BarcodeScanner, public navCtrl: NavController) { //get instances of these
+  }
+
+  async scanBarcode(){ //an asynchronous function
+
+    const results = await this.barcode.scan(); //use instance of barcode scanner to call the scan method. Store in results
+    this.resultsUI = await this.barcode.scan(); //results for user interface
+    console.log(results); 
   }
 
 }
