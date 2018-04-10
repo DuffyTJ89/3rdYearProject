7c10a3eee1d8861a48e7fd61d886ddd3f0336e7b
DuffyTJ89 <tomduffy89@gmail.com>
2018-04-10 18:05:47 +0100

    alert added for when barCode is sent to database

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import{BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
 
 import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'page-home',
@@ -12,10 +13,11 @@ export class HomePage {
 
   options: BarcodeScannerOptions; //options variable of type BarcodeScannerOptions
   resultsUI:{}; //used to dispaly the scanned results in the user interface 
+  generateBarcode; 
 
   //test = {test:'this is test data'};
 
-  constructor(private barcode: BarcodeScanner, public navCtrl: NavController, public db: AngularFireDatabase) { //get instances of these
+  constructor(private barcode: BarcodeScanner, public navCtrl: NavController, private alertCtrl: AlertController, public db: AngularFireDatabase) { //get instances of these
   
     //this.db.list('test').push(this.test);
   }
@@ -25,10 +27,25 @@ export class HomePage {
     const results = await this.barcode.scan(); //use instance of barcode scanner to call the scan method. Store in results
     this.resultsUI = await this.barcode.scan(); //results for user interface
     console.log(results); 
+    
+
   }
 
   async storeBarcode(){ //an asynchronous function
 
+  
+
     this.db.list('barcode').push(this.resultsUI);
+
+    let alert = this.alertCtrl.create({
+      title: 'Bar code stored',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  async getBarcode(){
+
+       
   }
 }
